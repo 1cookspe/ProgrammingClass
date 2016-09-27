eb32830d03dee57f7f71c5406e751dbbd6a6588b
1cookspe <spencercook@spencers-macbook-pro.local>
2016-09-27 09:37:05 -0400

    Initial Commit

---
+++ Lab00/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab00
+//
+//  Created by Spencer Cook on 2016-09-27.
+//  Copyright © 2016 Spencer Cook. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
